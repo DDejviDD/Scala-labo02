d9800c1339496778a776d198471e2ad24a04ddcd
Dejvid Muaremi <dejvid95@hotmail.com>
2019-04-04 19:44:15 +0200

    super-parser ?

--- Bot-tender/src/main/scala/Chat/Parser.scala
+++ Bot-tender/src/main/scala/Chat/Parser.scala
@@ -5,124 +5,124 @@ import Tree._
 
 // TODO - step 4
 class Parser(tokenizer: Tokenizer) {
+   import tokenizer._
 
-  import tokenizer._
+   var curTuple: (String, Token) = ("unknown", UNKNOWN)
 
-  var curTuple: (String, Token) = ("unknown", UNKNOWN)
+   def curValue: String = curTuple._1
+   def curToken: Token = curTuple._2
 
-  def curValue: String = curTuple._1
+   /** Reads the next token and assigns it into the global variable curTuple */
+   def readToken(): Unit = curTuple = nextToken()
 
-  def curToken: Token = curTuple._2
+   /** "Eats" the expected token, or terminates with an error. */
+   private def eat(token: Token): Unit = if (token == curToken) readToken() else expected(token)
 
-  /** Reads the next token and assigns it into the global variable curTuple */
-  def readToken(): Unit = curTuple = nextToken()
+   /** Complains that what was found was not expected. The method accepts arbitrarily many arguments of type TokenClass */
+   // TODO (BONUS): find a way to display the string value of the tokens (e.g. "BIERE") instead of their integer value (e.g. 6).
+   private def expected(token: Token, more: Token*): Nothing =
+      fatalError(" expected: " +
+         (token :: more.toList).mkString(" or ") +
+         ", found: " + curToken)
 
-  /** "Eats" the expected token, or terminates with an error. */
-  private def eat(token: Token): Unit = if (token == curToken) readToken() else expected(token)
+   def fatalError(msg: String): Nothing = {
+      println("Fatal error", msg)
+      new Exception().printStackTrace()
+      sys.exit(1)
+   }
 
-  /** Complains that what was found was not expected. The method accepts arbitrarily many arguments of type TokenClass */
-  // TODO (BONUS): find a way to display the string value of the tokens (e.g. "BIERE") instead of their integer value (e.g. 6).
-  private def expected(token: Token, more: Token*): Nothing =
-    fatalError(" expected: " +
-      (token :: more.toList).mkString(" or ") +
-      ", found: " + curToken)
-
-  def fatalError(msg: String): Nothing = {
-    println("Fatal error", msg)
-    new Exception().printStackTrace()
-    sys.exit(1)
-  }
-
-  /** the root method of the parser: parses an entry phrase */
-  def parsePhrases(): ExprTree = {
-
-    // Start the process by reading the first token.
-    readToken()
-
-    if (curToken == BONJOUR) eat(BONJOUR)
-    if (curToken == JE) {
-      eat(JE)
-      if (curToken == VOULOIR) {
-        eat(VOULOIR)
-        if (curToken == COMMANDE) {
-          eat(COMMANDE)
-
-
-          if (curToken == NUM) {
-            readToken()
-            Num()
-            if (curToken == PRODUIT) {
-              readToken()
-              Product()
-              if (curToken == MARQUE) {
-                readToken()
-                Marque()
-              } else if (curToken == OU) {
-                readToken()
-                Or()
-              } else if (curToken == ET) {
-                readToken()
-                And()
-              } else {
-                expected(MARQUE, OU, ET)
-              }
-            } else {
-              expected(PRODUIT)
+   def parsePrice() : ExprTree = {
+      if (curToken == COMBIEN){ eat(curToken)
+         if(curToken == COUTER){
+            eat(curToken)
+            while (curToken == NUM){ parseItems() }
+            ObtainPrice()
+         }
+         else { expected(COUTER) }
+      } else if(curToken == QUEL){ eat(curToken)
+         if (curToken == ETRE){ eat(curToken)
+            if(curToken == DETERMINANT){ eat(curToken)
+               if(curToken == PRIX){ eat(curToken)
+                  if(curToken == DETERMINANT){
+                     eat(curToken)
+                     while (curToken == NUM){ parseItems() }
+                     ObtainPrice()
+                  }
+                  else { expected(DETERMINANT) }
+               }
+               else { expected(PRIX) }
             }
-          } else {
-            expected(NUM)
-          }
+            else { expected(DETERMINANT) }
+         }
+         else { expected(ETRE) }
+      } else { expected(COMBIEN, QUEL) }
+   }
+   def parseItems() : ExprTree = {
+      if(curToken == NUM){
+         readToken()
+         Num()
+         if(curToken == PRODUIT){
+            readToken()
+            Product()
+            if(curToken == MARQUE){
+               readToken()
+               Marque()
+            } else if (curToken == OU){
+               readToken()
+               Or()
+            } else if (curToken == ET){
+               readToken()
+               And()
+            } else { expected(MARQUE, OU, ET) }
+         } else{ expected(PRODUIT) }
+      } else { expected(NUM) }
+   }
+   def parsePolite() : Boolean = { curToken == AIMER || curToken == VOULOIR }
 
-          Commande()
-        }
-        else if (curToken == SOLDE) {
-          readToken()
-          Balance()
-        }
-        else {
-          expected(COMMANDE, SOLDE)
-        }
-
-
-      } else if (curToken == ETRE) {
-        eat(ETRE)
-        if (curToken == ASSOIFFE) {
-          // Here we do not "eat" the token, because we want to have a custom 2-parameters "expected" if the user gave a wrong token.
-          readToken()
-          Thirsty()
-        }
-        else if (curToken == AFFAME) {
-          readToken()
-          Hungry()
-        }
-        else if (curToken == PSEUDO) {
-          readToken()
-          ReadOrAddUser()
-        }
-        else {
-          expected(ASSOIFFE, AFFAME, PSEUDO)
-        }
-      } /*else if(curToken == ME){
-            eat(ME)
-            if(curToken == APPELLE){
-               eat(APPELLE)
+   /** the root method of the parser: parses an entry phrase */
+   def parsePhrases() : ExprTree = {
+      // Start the process by reading the first token.
+      readToken()
+      if (curToken == BONJOUR) eat(BONJOUR)
+      if(curToken == COMBIEN || curToken == QUEL){ parsePrice() }
+      else if (curToken == JE ) {
+         eat(JE)
+         if(parsePolite()){ readToken()
+            if(curToken == COMMANDE){ readToken()
+               while (curToken == NUM){ parseItems() }
+               Commande()
+               Eol()
+            } else if(curToken == CONNAITRE){ readToken()
+               if(curToken == MON) { readToken()
+                  if(curToken == SOLDE){
+                     readToken()
+                     Balance()
+                  } else {expected(SOLDE)}
+               } else {expected(MON)}
+            } else { expected(CONNAITRE) }
+         }
+         else if (curToken == ETRE) { eat(ETRE)
+            if (curToken == ASSOIFFE) {
+               readToken()
+               Thirsty()
+            }
+            else if (curToken == AFFAME) {
+               readToken()
+               Hungry()
+            }
+            else if (curToken == PSEUDO){
+               readToken()
+               ReadOrAddUser()
+            } else { expected(ASSOIFFE, AFFAME, PSEUDO) }
+         } else if(curToken == ME){ eat(ME)
+            if(curToken == APPELLER){ eat(APPELLER)
                if (curToken == PSEUDO){
                   readToken()
                   ReadOrAddUser()
-               }
-               else {
-                  expected(PSEUDO)
-               }
-            } else {
-               expected(APPELLE)
-            }
-
-         }*/ else {
-        expected(VOULOIR, ETRE /*, ME*/)
-      }
-    }
-    else expected(BONJOUR, JE)
-  }
-
-
+               } else { expected(PSEUDO) }
+            } else { expected(APPELLER) }
+         }
+         else { expected(AIMER, VOULOIR, ETRE, ME) }
+      } else { expected(BONJOUR, COMBIEN, QUEL, JE) }
+   }
 }
