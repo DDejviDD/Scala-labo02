b592757a42005b66ee454d2159f97c65283255cc
manalito <aurelien.siu@heig-vd.ch>
2019-04-05 17:54:32 +0200

    in progress Tree

--- Bot-tender/src/main/scala/Chat/Tree.scala
+++ Bot-tender/src/main/scala/Chat/Tree.scala
@@ -1,57 +1,69 @@
 package Chat
 
+import Data.{Products, UsersInfo}
+
 // TODO - step 3
 object Tree {
 
-  /**
-    * This sealed trait represents a node of the tree and contains methods to compute it and write its output text in console.
-    */
-  sealed trait ExprTree {
-    /**
-      * Compute the price of the current node, then returns it. If the node is not a computational node, the method
-      * returns 0.0.
-      * For example if we had a "+" node, we would add the values of its two children, then return the result.
-      * @return the result of the computation
-      */
-    def computePrice: Double = {
+   /**
+     * This sealed trait represents a node of the tree and contains methods to compute it and write its output text in console.
+     */
+   sealed trait ExprTree {
 
-    }
+      /**
+        * Compute the price of the current node, then returns it. If the node is not a computational node, the method
+        * returns 0.0.
+        * For example if we had a "+" node, we would add the values of its two children, then return the result.
+        * @return the result of the computation
+        */
+      def computePrice: Double = this match {
+            // Example cases
 
-    /**
-      * Return the output text of the current node, in order to write it in console.
-      * @return the output text of the current node
-      */
-    def reply: String = this match {
-      // Example cases
-
-      case Num() => ""
-      case Beer() => ""
-      case Croissant() => ""
-      case Or() => "or"
-      case And() => "et"
-      case Chips() => ""
-      case Marque() => ""
+            case Product() => 0.0
+            case Or() => Math.min(leftNode.computePrice, rightNode.computePrice)
+            case And() => leftNode.computePrice + rightNode.computePrice
+      }
 
 
-      case Thirsty() => "Eh bien, la chance est de votre côté, car nous offrons les meilleures bières de la région !"
-      case Hungry() => "Pas de soucis, nous pouvons notamment vous offrir des croissants faits maisons !"
-    }
-  }
-  /**
-    * Declarations of the nodes' types.
-    */
-  // Example cases
-  case class Thirsty() extends ExprTree
-  case class Hungry() extends ExprTree
-  case class Num() extends ExprTree
-  case class Product() extends ExprTree
-  case class Beer() extends ExprTree
-  case class Croissant() extends ExprTree
-  case class Or() extends ExprTree
-  case class And() extends ExprTree
-  case class Chips() extends ExprTree
-  case class Marque() extends ExprTree
-  case class ReadOrAddUser() extends ExprTree
-  case class Commande() extends ExprTree
-  case class Balance() extends ExprTree
+      /**
+        * Return the output text of the current node, in order to write it in console.
+        * @return the output text of the current node
+        */
+      def reply: String = this match {
+         // Example cases
+
+
+         case Or() => "or"
+         case And() => "et"
+         case Login(name) => {
+            UsersInfo.setActiveUser(name)
+            "Bonjour, " + name
+         }
+         case Price(basket) => "Cela coûte CHF " + basket.computePrice
+
+         case Thirsty() => "Eh bien, la chance est de votre côté, car nous offrons les meilleures bières de la région !"
+         case Hungry() => "Pas de soucis, nous pouvons notamment vous offrir des croissants faits maisons !"
+      }
+   }
+   /**
+     * Declarations of the nodes' types.
+     */
+   // Example cases
+   case class Thirsty() extends ExprTree
+   case class Hungry() extends ExprTree
+   case class Login(username: String) extends ExprTree
+   case class Order(cart: ExprTree) extends ExprTree
+   case class Price(cart: ExprTree) extends ExprTree
+   case class Balance() extends ExprTree
+
+   case class Item(product: Product, brand: Brand = null) extends ExprTree {
+
+   }
+   case class Items(product: Product, number: Int) extends ExprTree {
+      override def toString: String = number.toString + " " + product.toString
+   }
+
+   case class Or(first: ExprTree, second: ExprTree) extends ExprTree
+   case class And(first: ExprTree, second: ExprTree) extends ExprTree
+
 }
